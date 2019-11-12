cd5899bac54ac34ad2bc764f5714d5c54743cbcd
Bispo <daniel@bisponet.com>
2019-11-12 10:40:24 -0300

    Fixing export object name at configs.js

--- config/configs.js
+++ config/configs.js
@@ -1,12 +1,12 @@
 const port = 4000
 const bodyParser = require('body-parser')
 const express = require('express')
-const server = express()
+const configs = express()
 
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(bodyParser.json())
+configs.use(bodyParser.urlencoded({ extended: true }))
+configs.use(bodyParser.json())
 
-server.listen(process.env.PORT || port, function () {
+configs.listen(process.env.PORT || port, function () {
     console.log('Listening on');
 })
 
